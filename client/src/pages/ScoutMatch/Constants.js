c561089c20b1c0521780953807dbba658e0f4b88
ARMAAN CHANDARANA <achandarana@bayviewglen.ca>
2025-04-03 08:39:32 -0400

    added dcmp to events list

--- client/src/pages/ScoutMatch/Constants.js
+++ client/src/pages/ScoutMatch/Constants.js
@@ -1,6 +1,6 @@
 //update in server too
 export const PRACTICE_EVENTS = []; //["2025week0", "2025onnew"];
-export const ATTENDING_EVENTS = ["2025ontor", "2025onsca"]; //, "2025ontor", "2025oncmp"];
+export const ATTENDING_EVENTS = ["2025ontor", "2025onsca", "2025oncmp1"]; //, "2025ontor", "2025oncmp"];
 
 export const FIELD_VIRTUAL_HEIGHT = 1610;
 export const FIELD_VIRTUAL_WIDTH = 3510;

81a12b20738b1844fb76c3d3d8cc940ae4dc4b2e
Armaan C <armaanchandarana1@gmail.com>
2025-11-03 19:29:03 -0500

    deploy thingy

--- server/index.js
+++ server/index.js
@@ -1,4 +1,8 @@
-import "dotenv/config";
+// Only load dotenv locally
+if (process.env.NODE_ENV !== "production") {
+  const dotenv = await import('dotenv');
+  dotenv.config(); // looks for .env in root by default
+}
 import path from "path";
 import { fileURLToPath } from "url";
 

189baee75fa6e9fadcc1f99b18b9af817924ddfa
Armaan C <armaanchandarana1@gmail.com>
2026-02-19 17:24:42 -0500

    fixed token

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -15,7 +15,7 @@ export const verifyToken = (req, res, next) => {
     const authHeader = req.headers["authorization"];
     if (!authHeader) return res.status(401).json({ message: "No token provided" });
 
-    const token = authHeader; // Expect "<token>"
+    const token = authHeader.split(" ")[1]; // Expect "Bearer <token>"
     if (!token) return res.status(401).json({ message: "Malformed token" });
 
     const user = jwt.verify(token, JWT_SECRET);

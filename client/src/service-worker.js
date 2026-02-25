c146de8a52c07d39817e213547e24a8d0f782276
Armaan C <armaanchandarana1@gmail.com>
2026-02-25 11:20:01 -0500

    Serve index.html when offline in the service worker

--- client/src/service-worker.js
+++ client/src/service-worker.js
@@ -2,9 +2,18 @@
 
 import { clientsClaim } from "workbox-core";
 import { precacheAndRoute } from "workbox-precaching";
+import { registerRoute } from "workbox-routing";
+import { NavigationRoute } from "workbox-routing";
 
 clientsClaim();
 self.skipWaiting();
 
-// This will be replaced at build time
 precacheAndRoute(self.__WB_MANIFEST);
+
+// 👇 Add this
+const handler = async ({ request }) => {
+  return caches.match('/index.html');
+};
+
+const navigationRoute = new NavigationRoute(handler);
+registerRoute(navigationRoute);
